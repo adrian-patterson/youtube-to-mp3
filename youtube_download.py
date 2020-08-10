97808a48635e276e6a9e6383b35a564d7c17b073
Adrian Patterson <patterson.n.adrian@gmail.com>
2020-08-10 13:37:28 -0400

    Added loop to allow user to enter multiple queries.

--- youtube_download.py
+++ youtube_download.py
@@ -2,20 +2,30 @@ import urllib.request
 import re
 from youtube_dl import YoutubeDL
 
+query_list = []
+
 while True:
-    user_search = input("Enter song:\t")
+	user_search = input("Enter song:\t")
 
-    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + user_search)
-    video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-    download_link = "https://www.youtube.com/watch?v=" + video_ids[0]
-    print("https://www.youtube.com/watch?v=" + video_ids[0])
+	if user_search == "done":
+		break
+	
+	user_search = user_search.replace(' ',"+")
+	query_list.append(user_search)	
 
-    ydl_opts = {
-                'format': 'bestaudio/best',
-                'postprocessors': [{'key': 'FFmpegExtractAudio',
-                                    'preferredcodec': 'mp3',
-                                    'preferredquality': '192',}],
-               }
+for query in query_list:
 
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([download_link])
+	html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + query)
+	video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+	download_link = "https://www.youtube.com/watch?v=" + video_ids[0]
+	print("https://www.youtube.com/watch?v=" + video_ids[0])
+
+	ydl_opts = {
+		'format': 'bestaudio/best',
+		'postprocessors': [{'key': 'FFmpegExtractAudio',
+			            'preferredcodec': 'mp3',
+			            'preferredquality': '192',}],
+	       }
+
+	with YoutubeDL(ydl_opts) as ydl:
+		ydl.download([download_link])
